84c98fcb23ee9c44c5676fd3a5da3b4376917486
Celiamf <celiamartinfern@gmail.com>
2020-11-15 14:35:24 +0100

    Convert CardIcons class component into function component -Celia branch

--- src/components/CardIcons.js
+++ src/components/CardIcons.js
@@ -1,19 +1,31 @@
 import React from "react";
 import "../stylesheets/layout/_cardIcons.scss";
 
-class MainSectionCard extends React.Component {
-  render() {
-    return (
-      <li>
-        <a href={this.props.href} id={this.props.id}>
-          <i
-            className={`fa ${this.props.icon} sectionOneCard__nav--logos js-socialLogos`}
-            aria-hidden="true"
-          ></i>
-        </a>
-      </li>
-    )
-  }
-}
+// class MainSectionCard extends React.Component {
+//   render() {
+//     return (
+//       <li>
+//         <a href={this.props.href} id={this.props.id}>
+//           <i
+//             className={`fa ${this.props.icon} sectionOneCard__nav--logos js-socialLogos`}
+//             aria-hidden="true"
+//           ></i>
+//         </a>
+//       </li>
+//     )
+//   }
+// }
 
+const MainSectionCard = (props) => {
+  return (
+    <li>
+      <a href={props.href} id={props.id}>
+        <i
+          className={`fa ${props.icon} sectionOneCard__nav--logos js-socialLogos`}
+          aria-hidden="true"
+        ></i>
+      </a>
+    </li>
+  );
+};
 export default MainSectionCard;
