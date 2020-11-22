79948fd6961ccb7b9baaac106e4363a1307d331e
Celiamf <celiamartinfern@gmail.com>
2020-11-22 18:40:40 +0100

    Update favicon

--- src/components/CardIcons.js
+++ src/components/CardIcons.js
@@ -1,19 +1,24 @@
 import React from "react";
 import "../stylesheets/layout/_cardIcons.scss";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 class CardIcons extends React.Component {
   render() {
     return (
       <li>
-        <a href={this.props.href} id={this.props.id} target="_blank">
+        <a
+          href={this.props.href}
+          id={this.props.id}
+          target="_blank"
+          rel="noreferrer"
+        >
           <i
             className={`fa ${this.props.icon} sectionOneCard__nav--logos js-socialLogos`}
             aria-hidden="true"
           ></i>
         </a>
       </li>
-    )
+    );
   }
 }
 
@@ -21,5 +26,5 @@ CardIcons.propTypes = {
   href: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
   icon: PropTypes.string.isRequired,
-}
+};
 export default CardIcons;
